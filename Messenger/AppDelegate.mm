ba4accd403d0957690ab0370bac7fd5f00d8738a
Josh Puckett <mith.puck@gmail.com>
2015-04-08 13:11:48 -0700

    made links open in default browser

--- Messenger/AppDelegate.mm
+++ Messenger/AppDelegate.mm
@@ -17,12 +17,25 @@
   self.window.contentView = webView;
   auto req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.messenger.com/"]];
   [webView.mainFrame loadRequest:req];
-    [[self.window standardWindowButton:NSWindowCloseButton]
-     setFrame:NSZeroRect];
-    [[self.window standardWindowButton:NSWindowMiniaturizeButton]
-     setFrame:NSZeroRect];
-    [[self.window standardWindowButton:NSWindowZoomButton]
-     setFrame:NSZeroRect];
+  [webView setPolicyDelegate:self];
+  [[self.window standardWindowButton:NSWindowCloseButton] setFrame:NSZeroRect];
+  [[self.window standardWindowButton:NSWindowMiniaturizeButton]setFrame:NSZeroRect];
+  [[self.window standardWindowButton:NSWindowZoomButton] setFrame:NSZeroRect];
+}
+
+- (void)webView:(WebView *)sender decidePolicyForNavigationAction:(NSDictionary *)actionInformation request:(NSURLRequest *)request frame:(WebFrame *)frame decisionListener:(id<WebPolicyDecisionListener>)listener {
+      if( [sender isEqual:self.window.contentView] ) {
+        [listener use];
+      }
+      else {
+        [[NSWorkspace sharedWorkspace] openURL:[actionInformation objectForKey:WebActionOriginalURLKey]];
+        [listener ignore];
+      }
+}
+
+- (void)webView:(WebView *)sender decidePolicyForNewWindowAction:(NSDictionary *)actionInformation request:(NSURLRequest *)request newFrameName:(NSString *)frameName decisionListener:(id<WebPolicyDecisionListener>)listener {
+      [[NSWorkspace sharedWorkspace] openURL:[actionInformation objectForKey:WebActionOriginalURLKey]];
+      [listener ignore];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {

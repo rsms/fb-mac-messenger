0f01f42dd2d4e33e9b9d4301d30600b10a5477df
Rasmus Andersson <rasmus@notion.se>
2015-04-08 12:07:52 -0700

    Initial Commit

---
+++ Messenger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Messenger
+//
+//  Created by Rasmus Andersson on 4/8/15.
+//  Copyright (c) 2015 Rsms. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Messenger/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Messenger
+//
+//  Created by Rasmus Andersson on 4/8/15.
+//  Copyright (c) 2015 Rsms. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}

a1f73f5b1165c51ced7a5181d7d412f49a2095b4
Rasmus Andersson <rasmus@notion.se>
2015-04-14 22:02:17 -0700

    Better solution for cmd-1..9 keyboard shortcut that does not rely on ANSI hardware key codes. Improves upon 530a8c1aca58eae83a87b3403be92d9d7701e308

--- Messenger/FBMApplication.mm
+++ Messenger/FBMApplication.mm
@@ -1,20 +1,21 @@
 #import "FBMApplication.h"
 #import "AppDelegate.h"
-#import "virtual-key-codes.hh"
 
 @implementation FBMApplication
 
-- (void)sendEvent:(NSEvent *)event {
+- (void)sendEvent:(NSEvent*)event {
   // Cmd-1 .. Cmd-9
-  using namespace VirtualKeyCodes;
   if (event.type == NSKeyDown && (event.modifierFlags & NSCommandKeyMask)) {
-    switch (event.keyCode) {
-      case ANSI_1: case ANSI_2: case ANSI_3: case ANSI_4: case ANSI_5: case ANSI_6: case ANSI_7: case ANSI_8: case ANSI_9: {
-        [((AppDelegate*)self.delegate) setActiveConversationAtIndex:event.characters];
-        return;
-      }
-      default: {
-        break;
+    auto chars = event.charactersIgnoringModifiers;
+    if (chars.length == 1) {
+      switch ([chars characterAtIndex:0]) {
+        case u'1' ... u'9': {
+          [((AppDelegate*)self.delegate) setActiveConversationAtIndex:event.characters];
+          return;
+        }
+        default: {
+          break;
+        }
       }
     }
   }

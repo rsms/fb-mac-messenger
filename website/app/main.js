f5b85aa138375518c426cb6965a9f571dd525b03
Rasmus Andersson <rasmus@notion.se>
2015-04-15 09:06:30 -0700

    Cleans up JS changes introduced in 29256916ab75fa2b2cd649c7fdbd2f62bfda41f8

--- website/app/main.js
+++ website/app/main.js
@@ -1,73 +1,57 @@
-// Let's not pollute the global namespace, shall we?
+// Note:
+//   window.MacMessengerVersion will be defined to a string e.g. to "0.1.2"
+//   window.MacMessengerGitRev will be defined to a string e.g. "abc917"
+
+window.MacMessenger = {
+  openGearMenu: function() {
+    this.gearButton.firstElementChild.dispatchEvent(
+      new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
+    );
+  },
+
+  logOut: function() {
+    // TODO: Actually "log out" instead of showing the menu
+    this.openGearMenu();
+  },
+
+
+  showSettings: function() {
+    this.openGearMenu();
+    document.querySelector(
+      'div.uiLayer.uiContextualLayerPositioner#js_1 > div.uiContextualLayer a'
+    ).dispatchEvent(
+      new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
+    );
+  },
+
+  composeNewMessage: function() {
+    document.querySelector('a[href="/new"]').dispatchEvent(
+      new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
+    );
+  },
+
+  focusSearchField: function() {
+    document.querySelector('input[placeholder~="Search"]').focus();
+  },
+
+  focusComposer: function() {
+    document.querySelector('div[contenteditable="true"]').focus();
+  },
+
+  selectConversationAtIndex: function(index) {
+    document.querySelector('li:nth-child('+index+') > [data-reactid]:first-child').click();
+  },
+
+};
+
+
+// Things that need the DOM to be loaded
 (function () {
-  // Note:
-  //   window.MacMessengerVersion will be defined to a string e.g. to "0.1.2"
-  //   window.MacMessengerGitRev will be defined to a string e.g. "abc917"
-
-  window.MacMessenger = {
-    openGearMenu: function() {
-      this.gearButton.firstElementChild.dispatchEvent(
-        new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
-      );
-    },
-
-    logOut: function() {
-      // TODO: Actually "log out" instead of showing the menu
-      this.openGearMenu();
-    },
-
-
-    showSettings: function() {
-      this.openGearMenu();
-      document.querySelector(
-        'div.uiLayer.uiContextualLayerPositioner#js_1 > div.uiContextualLayer a'
-      ).dispatchEvent(
-        new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
-      );
-    },
-
-    composeNewMessage: function() {
-      document.querySelector('a[href="/new"]').dispatchEvent(
-        new MouseEvent('click', {view:window, bubbles:true, cancelable:true})
-      );
-    },
-
-    focusSearchField: function() {
-      document.querySelector('input[placeholder~="Search"]').focus();
-    },
-
-    focusComposer: function() {
-      document.querySelector('div[contenteditable="true"]').focus();
-    },
-
-    selectConversationAtIndex: function(index) {
-      document.querySelector('li:nth-child('+index+') > [data-reactid]:first-child').click();
-    },
-
-  };
-
-  // The following two statements enable drag-and-drop file sending
-  document.addEventListener('dragover', function(ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = 'copy';
-  });
-  document.addEventListener('drop', function(ev) {
-    ev.stopPropagation();
-    ev.preventDefault();
-    document.querySelector('input[type="file"][name="attachment[]"]').files = ev.dataTransfer.files;
-  });
-
-  // Things that need the document to be fully loaded
-  onDocumentLoaded();
-  document.addEventListener('readystatechange', onDocumentLoaded);
-
-  var didAlreadyLoad = false;
-  function onDocumentLoaded() {
-    if (didAlreadyLoad || !(document.readyState === "complete" || document.readyState === "loaded" || document.readyState === "interactive")) {
-      return;
+  var onDocumentLoaded = function() {
+    if (document.readyState === "loading") {
+      return; // still loading
     }
-    didAlreadyLoad = true;
+    document.removeEventListener('readystatechange', onDocumentLoaded);
 
     // This fixes an annoying "beep" sound
     document.body.onkeypress = function (e) {
@@ -98,33 +82,23 @@
       });
       observer.observe(document.body, { attributes: false, childList: true, characterData: false });
     }
-  }
 
-  // window.MacMessenger.showErrorView = function(kind) {
-  //   document.body.innerText = '';
-  //   var e = document.createElement('p');
-  //   document.body.appendChild(e);
-  //   e.innerText = (kind == 'maintenance') ?
-  //     'Oh noes. It appears Messenger.com is down for maintenance. Please try again later.' :
-  //     'Oh snap. It looks like your connection is offline, please try again later.';
-  //   var s = e.style;
-  //   s.font = '18px helvetica-light';
-  //   s.lineHeight = '27px';
-  //   s.color = '#999';
-  //   s.margin = '0 auto';
-  //   s.width = '50%%';
-  //   s.textAlign = 'center';
-  //   s.margin = '0 auto';
-  //   s.marginTop = '100px';
-  //   s.marginBottom = '30px';
-  //   s.width = '235px';
-  //   s.height = '235px';
-  //   s.paddingTop = '250px';
-  //   s.backgroundRepeat = 'no-repeat';
-  //   s.backgroundPosition = 'top center';
-  //   s.backgroundImage = 'http://fbmacmessenger.rsms.me/error.png';
-  // };
+    // The following two statements enable drag-and-drop file sending
+    document.addEventListener('dragover', function(ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+      ev.dataTransfer.dropEffect = 'copy';
+    });
+    document.addEventListener('drop', function(ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+      document.querySelector('input[type="file"][name="attachment[]"]').files =
+        ev.dataTransfer.files;
+    });
 
-
-  console.log('app/main.js loaded');
+  };
+  document.addEventListener('readystatechange', onDocumentLoaded);
+  onDocumentLoaded();
 })();
+
+console.log('app/main.js loaded');

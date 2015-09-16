7adfa9d25e6ca52ec81e5a9674b2a8de1336930f
Rasmus Andersson <rasmus@notion.se>
2015-09-16 11:02:00 -0700

    Changes to build configuration
    Targets SDK 10.10 explicitly, as 10.11 is soon released and we want to support OS X >=10.10.
    
    Closes #189

--- Messenger/AppDelegate.h
+++ Messenger/AppDelegate.h
@@ -1,6 +1,11 @@
 #import <WebKit/WebKit.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate, NSWindowDelegate>
+// ISSUE 189: When we switch to build against the 10.11 SDK, the following interfaces
+// have been converted to protocols, requiring us to specify adherence:
+//  - WebPolicyDelegate
+//  - WebFrameLoadDelegate
+//  - WebUIDelegate
 
 - (IBAction)checkForUpdates:(id)sender;
 - (void)setActiveConversationAtIndex:(NSString *)index;

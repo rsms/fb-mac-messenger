6ad205b2cee30e79ee9ed052d83564f68c7ed93b
Dave Pagurek <dave@davepagurek.com>
2017-02-17 21:14:51 -0500

    Fixes for overflow scrolling and sidebar alignment (#330)
    * Fixes for overflow scrolling and sidebar alignment
    
    * Update component names and widths

--- website/app/main.js
+++ website/app/main.js
@@ -493,11 +493,12 @@
         observer.observe(document.body, { childList: true });
       }
     }
-    styleComponent("Messenger", {
-      "(max-width: 640px)": function(el, matches) {
+    styleComponent("MessengerReact", {
+      "(max-width: 700px)": function(el, matches) {
+        el.style.minWidth = "0";
 
         // Allow sidebar to go smaller
-        el.firstElementChild.style.minWidth = matches ? null : "280px";
+        el.firstElementChild.style.minWidth = matches ? "0" : "280px";
         var newConversation = el.querySelector("a[href='/new']");
         newConversation.style.marginRight = matches ? "-109px" : null;
         newConversation.style.float = matches ? "right" : null;
@@ -506,7 +507,7 @@
       }
     });
     styleComponent("MessengerRecentContainer", {
-      "(max-width: 640px)": function(el, matches) {
+      "(max-width: 700px)": function(el, matches) {
         Array.prototype.forEach.call(el.querySelectorAll("ul li"), function(thread) {
           Array.prototype.forEach.call(
             thread.querySelectorAll("div[aria-label='Conversation actions'], div[aria-label='Actions'] img"),
@@ -535,7 +536,7 @@
       subtree: true
     });
     styleComponent("MessengerDetailView", {
-      "(max-width: 640px)": function(el, matches) {
+      "(max-width: 700px)": function(el, matches) {
         // Move border from entire right pane to just the conversation
         el.style.borderLeft = matches ? "0" : null;
         el.querySelector(":scope > div:last-child").style.borderLeft =
@@ -555,11 +556,15 @@
       }
     });
     styleComponent("MessengerBanner", {
-      "(max-width: 640px)": function(el, matches) {
+      "(max-width: 700px)": function(el, matches) {
         el.style.display = 'none';
       }
     });
- 
+    styleComponent("MessengerThreadInfoPanelContainer", {
+      "(max-width: 700px)": function(el, matches) {
+        el.style.webkitBoxDirection = 'normal';
+      }
+    });
 
   };
   document.addEventListener('readystatechange', onDocumentLoaded);
